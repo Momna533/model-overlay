69f8576da6addc53e516fdcc07f74d7538098855
Momna533 <108815490+Momna533@users.noreply.github.com>
2023-03-27 19:36:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+/*const btn = document.querySelector(".btn");
+const overlay = document.querySelector(".overlay");
+const close = document.querySelector(".close");
+
+btn.addEventListener("click", ()=>{
+    overlay.classList.add("open_overlay")
+})
+close.addEventListener("click", ()=>{
+    overlay.classList.remove("open_overlay")
+})*/
+
+const btn = document.querySelector(".btn");
+const overlay = document.querySelector(".overlay");
+const close = document.querySelector(".close");
+
+btn.addEventListener("click",()=>{
+    overlay.classList.add("open_overlay")
+})
+close.addEventListener("click",()=>{
+    overlay.classList.remove("open_overlay")
+})
